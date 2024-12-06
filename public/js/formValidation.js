9cf669a12f0d60af790f37a218182184ac0802b6
CallThyCJ <135438020+CallThyCJ@users.noreply.github.com>
2024-12-06 14:56:02 +0000

    Updated email regex
    I updated the email client side regex to make sure the user can't enter an email that doesn't have a .com or something like that on the end.

--- public/js/formValidation.js
+++ public/js/formValidation.js
@@ -44,7 +44,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 function validation(event, formType) {
-    const emailRegex = /^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)$/;
+    const emailRegex = /^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z]{2,})(?:\.[a-zA-Z]{2,})?$/;
     const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
     const usernameErrors = document.getElementsByClassName("usernameError");
     const emailErrors = document.getElementsByClassName("emailError");
